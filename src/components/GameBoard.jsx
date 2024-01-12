8f7be0bb65872cb593d91f842f28336cf02f3b7c
wangkodok <wangkodok@gmail.com>
2024-01-12 18:46:15 +0900

    보드판 클릭 및 상태 불변성

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,16 +7,41 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setGameBoard((prevGameBoard) => {
+    //   prevGameBoard[rowIndex][colIndex] = "X";
+    //   return prevGameBoard;
+    // });
+
+    setGameBoard((prevGameBoard) => {
+      const updateBoard = [
+        ...prevGameBoard.map((innerArray) => {
+          return [...innerArray];
+        }),
+      ];
+      updateBoard[rowIndex][colIndex] = "X";
+      return updateBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => {
+      {gameBoard.map((row, rowIndex) => {
         return (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => {
                 return (
                   <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button
+                      onClick={() => {
+                        return handleSelectSquare(rowIndex, colIndex);
+                      }}
+                    >
+                      {playerSymbol}
+                    </button>
                   </li>
                 );
               })}
