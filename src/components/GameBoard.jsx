c355e9fbded1a835ee25b94e01bd2f15d85adcce
wangkodok <wangkodok@gmail.com>
2024-01-18 19:15:04 +0900

    State(상태) 관리 간소화 및 불필요한 State(상태) 분별 그리고 조건적 버튼 비활성화

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -48,6 +48,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                       onClick={() => {
                         onSelectSquare(rowIndex, colIndex);
                       }}
+                      disabled={playerSymbol !== null}
                     >
                       {playerSymbol}
                     </button>
